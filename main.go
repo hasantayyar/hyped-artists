46470fdb442c14274e6ea15d13358c18c700a531
hasantayyar <tayyar.besik@gmail.com>
2014-01-23 17:20:14 +0200

    get hyped artists from last.fm

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"log"
 	"io/ioutil"
 	"fmt"
+	"regexp"
 )
  
 const url = "http://www.last.fm/charts/artists/hyped/place/all"
 func main() {
+	
+
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("Failed to fetch %s", url)
@@ -19,5 +22,18 @@ func main() {
 	if err != nil {
 		log.Printf("Failed to read the response body!")
 	}
-	fmt.Println(string(body))
+
+
+	re, _ := regexp.Compile(`<span class="rankItem-title">(.*)</span>`)
+    titles := re.FindAllStringSubmatch(string(body), -1)
+
+
+
+	re2, _ := regexp.Compile(`href="(.*)"(.*)class="rankItem-blockLink"`)
+    urls := re2.FindAllStringSubmatch(string(body), -1)
+
+    for index,element := range titles {
+    	fmt.Printf("%v\t\t", element[1])
+    	fmt.Printf("http://last.fm/%v \n", urls[index][1])
+	}
 }
